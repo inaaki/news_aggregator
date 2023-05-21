58e3f48123bf128d58377555bd33f1f71d440d20
Inzamul Akib <77077129+inaaki@users.noreply.github.com>
2023-05-21 22:12:51 +0600

    feat: add form handler to Login component

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function Login() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div class="min-h-screen flex items-center justify-center bg-gray-100">
-      <div class="bg-white shadow-md rounded px-8 py-6">
-        <h2 class="text-2xl font-bold mb-6">Login</h2>
-        <form>
-          <div class="mb-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-md rounded px-8 py-6">
+        <h2 className="text-2xl font-bold mb-6">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
             >
-              Username
+              Email
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
-              type="text"
-              placeholder="Enter your username"
+              type="email"
+              placeholder="Enter your email"
+              {...register('email')}
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
             >
               Password
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
               placeholder="Enter your password"
+              {...register('password')}
             />
           </div>
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
             >
               Sign In
             </button>
