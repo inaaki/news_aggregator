2115221e11ddbd38e75f14f51ad8b3ca49a8fe69
Inzamul Akib <77077129+inaaki@users.noreply.github.com>
2023-05-21 19:54:30 +0600

    feat: create login form

---
+++ src/assets/components/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+      <div class="bg-white shadow-md rounded px-8 py-6">
+        <h2 class="text-2xl font-bold mb-6">Login</h2>
+        <form>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
