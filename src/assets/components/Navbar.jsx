82bac42b04059454615c9226cf7ea1b20b154b7f
Inzamul Akib <akib@impelitsolutions.com>
2023-05-23 20:20:05 +0600

    feat: create SearchBar component

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 export default function Navbar() {
   // routing based on user login or logout
   const loggedIn = false
   return (
-    <nav class="bg-gray-800">
-      <div class="container mx-auto px-4">
-        <div class="flex items-center justify-end h-16">
-          <div class="flex items-center">
-            <div class="ml-4">
+    <nav className="bg-gray-800">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <SearchBar />
+          <div className="flex items-center">
+            <div className="ml-4">
               {loggedIn ? (
-                <a class="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
+                <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
                   Logout
                 </a>
               ) : (
                 <>
-                  <a class="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
+                  <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
                     Login
                   </a>
-                  <a class="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
+                  <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md cursor-pointer">
                     Sign Up
                   </a>
                 </>
---
+++ src/assets/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+export default function SearchBar() {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="What kind of news?"
+        className="px-4 pr-10 py-1 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+      />
+      <button className="absolute inset-y-0 right-0 px-2 text-gray-600 hover:text-gray-800 rounded-r-md">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
